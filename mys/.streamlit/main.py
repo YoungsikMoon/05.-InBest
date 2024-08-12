d4ce3a39df6b03e4fa78d9ec6408f089f65b7e8f
문영식 <moonyoungsik95@gmail.com>
2024-08-12 09:39:59 +0900

    .

--- mys/.streamlit/main.py
+++ mys/.streamlit/main.py
@@ -1,13 +1,15 @@
-from utils import start_streamlit, make_session, print_messages, input_output
+import utils as ut
+# import importlib
+# importlib.reload(ut)
 
 # 스트림릿 시작
-start_streamlit("문영식")
+ut.start_streamlit("문영식")
 
 # 세션이 없으면 만들어라
-make_session()
+ut.make_session()
 
 # 이전 대화기록 출력해주는 코드
-print_messages()
+ut.print_messages()
 
 # 챗봇 활성화
-input_output()
+ut.input_output()
